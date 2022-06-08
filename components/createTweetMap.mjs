5c2c76063c51031053d28290cb50d4dbbd334fec
JD Calvelli <jdcalvelli@gmail.com>
2022-06-08 14:10:13 -0500

    refactoring createTweetMap so that it can take in arguments for lat, lng

--- components/createTweetMap.mjs
+++ components/createTweetMap.mjs
@@ -1,13 +1,7 @@
 import puppeteer from 'puppeteer'
 
-function createTweetMap(lat, lng, joinedID) {
-    //create the javascript string that will be injected to puppeteer
+async function createTweetMap(lat, lng, forecastID, updateID) {
 
-    // going back to injecting scripts with puppeteer and taking screenshots
-    // bc we can inject the cdn version of leaflet!
-
-    //execution of puppeteer
-    (async () => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         // await page.goto('https://www.google.com')
@@ -15,7 +9,7 @@ function createTweetMap(lat, lng, joinedID) {
         await page.addStyleTag({ url: "https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" })
         await page.addScriptTag({ url: "https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" })
         //injecting our map code
-        await page.evaluate(() => {
+        await page.evaluate((lat, lng, forecastID, updateID) => {
             //adding the div element we will need for leaflet
             let mapDiv = document.createElement('div')
             mapDiv.id = 'map'
@@ -26,22 +20,24 @@ function createTweetMap(lat, lng, joinedID) {
             document.querySelector('body').appendChild(mapDiv)
 
             //actually utilizing leaflet
-            let map = L.map('map').setView([51.505, -0.09], 13);
+            let map = L.map('map').setView([lat, lng], 6);
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
-            L.marker([51.5, -0.09]).addTo(map)
-                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-                .openPopup();
-        })
+            L.marker([lat, lng]).addTo(map)
+
+        }, lat, lng)
         
         //have to wait to let the tiles load
         await page.waitForTimeout(1500)
         
-        await page.screenshot({ path: '../mapImages/example.png' });
+        await page.screenshot({ path: `../mapImages/${forecastID + "-" + updateID}.png` })
 
-        await browser.close();
-    })();
+        await browser.close()
+
 }
 
-createTweetMap()
+createTweetMap(41.795, -87.593, 1, 1)
+createTweetMap(40.914, -73.808, 2, 1)
+
+//export { createTweetMap }
